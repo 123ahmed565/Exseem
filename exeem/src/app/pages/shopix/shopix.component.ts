1fa24e2023519975fa34cf997aff073ae36275a5
123ahmed565 <ahmedseragmohamed@gmail.com>
2022-07-07 16:32:43 +0200

    add popular application

--- exeem/src/app/pages/shopix/shopix.component.ts
+++ exeem/src/app/pages/shopix/shopix.component.ts
@@ -1,4 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import swiper from 'swiper';
+import SwiperCore, {Autoplay} from 'swiper';
+SwiperCore.use([Autoplay]);
+import Swiper, { SwiperOptions } from 'swiper';import { SwiperComponent } from 'swiper/angular';
 
 @Component({
   selector: 'app-shopix',
@@ -6,6 +10,19 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./shopix.component.css']
 })
 export class ShopixComponent implements OnInit {
+
+  @ViewChild('swiperSlideShow') swiperSlideShow!: SwiperComponent;
+  config: SwiperOptions = {};
+
+  ngAfterViewInit(): void
+  {
+    this.config = {
+      autoplay: {
+        delay: 6000,
+        disableOnInteraction: false
+      }
+    }
+  }
 
   constructor() { }
 
